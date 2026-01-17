38f0d5856a02861c829d18a2ba9bae69d7807d6c
REE_TISH <reetishk798@gmail.com>
2026-01-17 23:14:45 +0530

    added register with google option

--- src/account/RegisterPage.jsx
+++ src/account/RegisterPage.jsx
@@ -4,6 +4,7 @@ import toast from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 import { LOCAL_URL } from '../utils/api'
 import { ToastErrorMessage, ToastSuccessMessage } from '../utils/toastMessages'
+import GoogleLoginButton from './LoginWithGoogle'
 
 
 function RegisterPage() {
@@ -27,12 +28,6 @@ function RegisterPage() {
     }));
     }
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            toast("Make sure to remember USER ID")
-        },1000)
-        
-    },[])
 
     const handleCreateUser = async() => {
 
@@ -79,9 +74,9 @@ function RegisterPage() {
 
     return (
         <div className='min-h-screen bg-gradient-to-l flex justify-center items-center from-gray-900 to-black'>
-            <div className='bg-black border rounded-2xl border-slate-600 p-8'>
-                <div className='flex justify-center items-center mb-6'>
-                    <h1 className='text-white text-xl'>Register !!</h1>
+            <div className='bg-black border rounded-2xl flex flex-col gap-2 border-slate-600 px-8 py-3'>
+                <div className='flex justify-center items-center '>
+                    <h1 className='text-white text-xl font-thin'>Register !!</h1>
                 </div>
                 <div className='flex flex-col gap-4'>
                     {/* User_ID */}
@@ -142,6 +137,7 @@ function RegisterPage() {
                         />
                     </div>
                     <div>
+                    
                         <button 
                             onClick={handleCreateUser} 
                             className='w-full text-white cursor-pointer  hover:bg-gray-800 p-2 rounded-xl bg-gray-900 transition-colors'
@@ -150,6 +146,7 @@ function RegisterPage() {
                         </button>
                     </div>
                 </div>
+                <GoogleLoginButton/>
             </div>
         </div>
     )
